f18b5c183bae70fa1bd205c861b4a309ee3e5091
r-dvl <rauldel.valle.lima@gmail.com>
2025-04-01 12:11:44 +0200

    Add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {ProfileComponent} from './views/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'profile', component: ProfileComponent },
+];

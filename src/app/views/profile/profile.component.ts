e00d0159352cd64de06643cab62c58eba58133b7
r-dvl <rauldel.valle.lima@gmail.com>
2025-04-01 13:21:01 +0200

    Add Users service

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  title = 'frontend';
+  users: any[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (data) => this.users = data,
+      error: (err) => console.error('Error fetching users:', err)
+    });
+  }
 }

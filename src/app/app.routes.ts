fecbe7a9d55bded937515950884eb54ede68dfb4
r-dvl <rauldel.valle.lima@gmail.com>
2025-04-01 12:31:42 +0200

    Add page not found view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
 import {ProfileComponent} from './views/profile/profile.component';
+import {PageNotFoundComponent} from './views/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'profile', component: ProfileComponent },
+    { path: '**', component: PageNotFoundComponent },
 ];

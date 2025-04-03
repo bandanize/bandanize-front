36be33819749a253ac77b32a65db2ad74cd1e9e9
r-dvl <rauldel.valle.lima@gmail.com>
2025-04-03 19:29:44 +0200

    Create bands view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProfileComponent } from './views/profile/profile.component';
+import { BandsComponent } from './views/bands/bands.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'bands', component: BandsComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/views/bands/bands.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-bands',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bands.component.html',
+  styleUrls: ['./bands.component.css']
+})
+export class BandsComponent {
+
+}
